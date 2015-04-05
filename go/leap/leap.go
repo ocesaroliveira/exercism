6413627e624596e4010badc4b9eeb5f11dcd1690
César Oliveira <cesar.o.almeida@gmail.com>
2015-04-05 14:09:03 -0300

    Finish Leap Go test

---
+++ go/leap/leap.go
@@ -0,0 +1,16 @@
+package leap
+
+const TestVersion = 1
+
+func IsLeapYear(year int) bool {
+	if year%4 == 0 {
+		if year%100 == 0 {
+			if year%400 == 0 {
+				return true
+			}
+			return false
+		}
+		return true
+	}
+	return false
+}
