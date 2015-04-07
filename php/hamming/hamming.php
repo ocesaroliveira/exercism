9efecc47cdb872cf2ebfda4d5e11b790aae8c1bd
César Oliveira <cesar.o.almeida@gmail.com>
2015-04-07 10:59:19 -0300

    Upgrade code
    I discussed with a developer and he proves that resolution is better.
    
    Look the discussing in http://exercism.io/submissions/5722c245129745098e2a92d4ae2ec2d0

--- php/hamming/hamming.php
+++ php/hamming/hamming.php
@@ -5,5 +5,11 @@ function distance($a, $b)
     if (strlen($a) != strlen($b)) {
         throw new InvalidArgumentException('DNA strands must be of equal length.');
     }
-    return count(array_diff_assoc(str_split($a), str_split($b)));
+    $count = 0;
+    for ($i = 0; $i < $aLength; $i++) {
+        if ($a[$i] !== $b[$i]) {
+            $count++;
+        }
+    }
+    return $count;
 }
