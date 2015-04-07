184a6a6c3338b8daa239d73a7f9af9407e65e4f6
César Oliveira <cesar.o.almeida@gmail.com>
2015-04-07 09:45:55 -0300

    Fix strlen bug

--- php/hamming/hamming.php
+++ php/hamming/hamming.php
@@ -2,7 +2,7 @@
 
 function distance($a, $b)
 {
-    if (strlength($a) != strlength($b)) {
+    if (strlen($a) != strlen($b)) {
         throw new InvalidArgumentException('DNA strands must be of equal length.');
     }
     return count(array_diff_assoc(str_split($a), str_split($b)));
